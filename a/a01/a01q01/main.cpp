f6d09d7c9e0be252f1988c694499daebcbd45695
Nile <nile.weadick@gmail.com>
2022-01-13 10:07:00 -0600

    dff

---
+++ a/a01/a01q01/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int input;
+
+int main(){
+
+    while(true){
+        cout << "\n>>>"; 
+        cin >> input;
+        cout << input;
+    }
+
+    return 0;
+}
