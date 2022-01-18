c1438aa1db0367df62d22b1a01b6784226ee7633
Nile <nile.weadick@gmail.com>
2022-01-18 11:54:47 -0600

    need to test for octals

--- a/a01/a01q01/main.cpp
+++ a/a01/a01q01/main.cpp
@@ -1,36 +1,48 @@
 #include <iostream>
 
 using namespace std;
+
+// user input string
 string input;
 
 
 int main(){
 
+    string output;
+
     while(true){
+
+        // prompt & input
         cout << "\n>>>";
         cin >> input;
 
-            char p[input.length()];
-            for(int i = 0; i <sizeof(input); i++){
+        // convert input string to char array
+        char p[input.length()];
+        for(int i = 0; i <sizeof(input); i++){
             p[i] = input[i];
-            }
-
-
-            int num = (int) p[0];
-            if(num == 0){
-                cout << " no ocatlas";
-            }
-
-
-        try{
-
-            stoi(input);
-            cout << input;
         }
 
-        catch (const exception& e){
-            cout << "ERROR: " <<"'" << input << "'" << " is neither a value nor found in the symtable";
+        for(int i = 0; i < sizeof(p); i++){
+
+            if(!isdigit(p[i])){
+                cout << "ERROR: " << input << " is neither a value nor found in the symtable";
+                break;
+            }
+
+            else if(p[0] == 0){
+                cout << "ERROR: I don't understand octals yet";
+                break;
+            }
+
+            else{
+                output += p[i];
+            }
+            
         }
+
+        cout << output;
+        output = "";
+
     }
 
     return 0;
