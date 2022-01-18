ae67922f0804fd867651a69f4900755393ea2f43
Nile <nile.weadick@gmail.com>
2022-01-18 10:38:35 -0600

    blah

--- a/a01/a01q01/main.cpp
+++ a/a01/a01q01/main.cpp
@@ -3,17 +3,33 @@
 using namespace std;
 string input;
 
+
 int main(){
 
     while(true){
-        cout << "\n>>>"; 
+        cout << "\n>>>";
         cin >> input;
 
-        if(!isdigit(input)){
-            cout << "\"" << input << "\"" << "is neither a value nor found in the symtable";
-        }
-        else{
+            char p[input.length()];
+            for(int i = 0; i <sizeof(input); i++){
+            p[i] = input[i];
+            }
+
+
+            int num = (int) p[0];
+            if(num == 0){
+                cout << " no ocatlas";
+            }
+
+
+        try{
+
+            stoi(input);
             cout << input;
+        }
+
+        catch (const exception& e){
+            cout << "ERROR: " <<"'" << input << "'" << " is neither a value nor found in the symtable";
         }
     }
 
