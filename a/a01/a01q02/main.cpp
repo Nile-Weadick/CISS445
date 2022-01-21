fd15c1563010f72299840f71c266d3b7ef96f4d3
Nile <nile.weadick@gmail.com>
2022-01-21 14:09:48 -0600

    jan21

--- a/a01/a01q02/main.cpp
+++ a/a01/a01q02/main.cpp
@@ -1,37 +1,96 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <list>
+
+using namespace std;
+
+string symtable[5][2];
 
 bool isInt(string x){
+    string a = "";
+    for(int i=0; i<x.length(); i++){
+        if(isdigit(x[i])){
+            a += x[i];
+        }
+    }
+
+    try{
+        isdigit(stoi(x));
+        return true;
+    }
+
+    catch(exception& e){
+        return false;
+    }
+}
+
+bool isOct(string x){
+
+    int num;
+    string c =  string(1,x[0]);
+    try{
+        num = stoi(c);
+    }
+
+    catch(const std::exception& e){
+        return false;
+    }
+
+    if(x.length() > 1 && num == 0){
+        return true;
+    }
+
+    else{
+        return false;
+    }
+}
+
+bool checkSymtable(string var){
+    for(int i=0; i<sizeof(symtable); i++){
+
+        if(symtable[i][0] == var){
+            return true;
+        }
+    }
+    return false;
+}
+
+void evalExp(string exp){
     
 }
-
-bool isOctal(string x){
-
-}
-
-bool checkSymtable(sting x){
-    
-}
-
-bool eval_expre(string x){
-    // for the = sign
-    // LHS as variable
-    // RHS as value
-
-    //insert into symtable
-
-    //true yes we are able to eval
-    //false not able to eval
-}
-
 
 int main(){
-    std::string input;
+    string input;
 
     while(true){
-        std::cout << "\n>>> ";
-        std::cin >> input;
+        cout << "\n>>> ";
+        cin >> input;
+
+        if(isInt(input)){
+
+            if(isOct(input)){
+                cout << "ERROR: I don't understand octals yet";
+            }
+            else{
+                for(int i=0; i<=input.length(); i++){
+                    if(isdigit(input[i])){
+                        cout << input[i];
+                    }
+                }
+            }
+        }
+
+        else{
+
+            if(checkSymtable(input)){
+
+            }
+
+            else{
+                cout << "ERROR: " << input << " is neither a value nor found in the symtable";
+            }
+        }
     }
 
     return 0;
