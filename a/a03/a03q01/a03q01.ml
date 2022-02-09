c211fc39d1a565037626ff76714a83e4cddb2220
Nile <nile.weadick@gmail.com>
2022-02-09 17:59:21 -0500

    feb9

--- a/a03/a03q01/a03q01.ml
+++ a/a03/a03q01/a03q01.ml
@@ -14,10 +14,18 @@ let rec power = fun x -> fun n -> if n == 0 then 1 else x * power x (n-1);;
 using this recursion:**)
 let rec power2 = fun x -> fun n -> 
     if n == 0 then 1 
-    else if (n mod 2) == 0 then x * power x (n-1)
-    else x * power x (n-1);;
+    else if (n mod 2) == 0 then power x (n/2) * power x (n/2)
+    else x * power x ((n-1)/2) * power x ((n-1)/2);;
 
 (**Q3. Write a function sum_to such that (sum_to n) computes the sum from 0 to n. If n is
 negative, then 0 is returned.**)
+let rec sum_to = fun n -> if n < 0 then 0 else n + sum_to (n-1);;
 
-print_int(power2 (-3) 3);;
+(**Q4. Write a function isprime such that (isprime n) is true if and only if n is a prime.**)
+let rec isprime = fun n -> if n > isprime n-1 then 1 else 0;;
+
+(**Q5. Write a function head that returns the first element of a list. (Ignore the case where
+the list passed in is empty).**)
+let head;;
+
+
