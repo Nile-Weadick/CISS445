60f2cdd9cbcfe342fecd5d35b031972787658011
Nile <nile.weadick@gmail.com>
2022-02-14 19:59:19 -0500

    feb14

--- a/a03/a03q01/a03q01.ml
+++ a/a03/a03q01/a03q01.ml
@@ -22,10 +22,22 @@ negative, then 0 is returned.**)
 let rec sum_to = fun n -> if n < 0 then 0 else n + sum_to (n-1);;
 
 (**Q4. Write a function isprime such that (isprime n) is true if and only if n is a prime.**)
-let rec isprime = fun n -> if n > isprime n-1 then 1 else 0;;
+let rec isprime = fun n -> let rec checkZero x d = match d with
+| 1 -> true    
+| _ -> (x mod d <> 0) && checkZero x (d-1)
+in match n with
+| 0 | 1 -> false
+| _ -> checkZero n (n-1) ;;
 
 (**Q5. Write a function head that returns the first element of a list. (Ignore the case where
 the list passed in is empty).**)
-let head;;
+let head = fun x -> match x with x::xs -> x;;
 
+(**Q6. Write a function tail that returns the elements of a list (as a list) without the head. If
+the list is empty, the empty list is returned. Use pattern matching.**)
+let tail = fun x -> match x with [] -> [] | x::xs -> xs;;
+
+(**Q7. Write a function second that returns the second element of a list. (Hint: Use what you
+have already written. Ignore the case where the list does not have a second element)**)
+let second = fun x -> head (tail (x));;
 
