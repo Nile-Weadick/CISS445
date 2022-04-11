4ad81be419a7a7ed76e7628aa2a901e7c125f034
Nile <nile.weadick@gmail.com>
2022-04-11 14:07:40 -0400

    April 11, 2022

---
+++ a/a07/a07q01/a07q01.py
@@ -0,0 +1,18 @@
+#Name: Nile Weadick
+#File a07q01.py
+
+#Q1
+N = NFA(alphabet=[],
+ states=[],
+ start=None,
+ accepts=[],
+ transitions=[])
+
+
+
+#Q2
+M = DFA(alphabet=[],
+ states=[],
+ start=None,
+ accepts=[],
+ transitions=[])
